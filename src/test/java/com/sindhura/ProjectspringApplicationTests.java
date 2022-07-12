d44302c044d2356ee57d9d98a25f7bf9f3e4323c
GANTASINDHURA <GANTASINDHURA83@GMAIL.com>
2022-07-12 19:48:31 +0530

    first commit

---
+++ src/test/java/com/sindhura/ProjectspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sindhura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
